af609aa276411470f1c6638607d209eeb07f6f23
ThugUnicorn713 <victoriaabeale@gmail.com>
2024-09-23 10:39:27 +0100

    test task

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+            float someFloat = 3.398084f;
+            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.ReadKey();
+
         }
     }
 }
