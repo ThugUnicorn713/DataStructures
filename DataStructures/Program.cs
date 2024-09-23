c31618b9dbb4f77bd766bc5a642b0bdee8b4ec87
ThugUnicorn713 <victoriaabeale@gmail.com>
2024-09-23 11:27:41 +0100

    added class, and List

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,13 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Movie deadpool3;
+            deadpool3.title = "Deadpool & Wolverine";
+            deadpool3.releaseYear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
+            
+            Console.WriteLine(deadpool3.title + " (" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
             Console.ReadKey();
 
-            float someFloat = 3.398084f;
-            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.WriteLine("\n\n");
+            //Console.Clear();
+
+            List<TVshow> shows = new List<TVshow>();
+            shows.Add(new TVshow(" The Lord Of The Rings: The Rings of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVshow(" The Boys", 2019, 7.99, MovieRating._18 ));
+            shows.Add(new TVshow(" The Matrix", 1999, 5.99, MovieRating._18 ));
+
+            for (int i = 0; i < shows.Count; i++) 
+            {
+                Console.Write(shows[i].ToString());
+            }
+
             Console.ReadKey();
 
+
+        }
+
+        public enum MovieRating
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18
+        }
+
+        public struct Movie 
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
+        
         }
     }
 }
---
+++ DataStructures/TVshow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataStructures.Program;
+
+namespace DataStructures
+{
+    internal class TVshow
+    {
+        public string title = "Untitled";
+        public int releaseYear;
+        public double price;
+        public MovieRating rating;
+        
+        public TVshow( string title, int releaseYear, double price, MovieRating rating) 
+        {
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.price = price;
+            this.rating = rating;
+            
+        }
+
+        public override string ToString()
+        {
+            return title + " (" + releaseYear + ") Rated " + rating + " . . .£" + price.ToString("0.00");
+        }
+    }
+}
